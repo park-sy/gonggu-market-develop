4a75950a31b46cdf6f94d1bc01124186c2740dc7
empodi <kglein9513@gmail.com>
2022-12-08 12:50:38 +0900

    Merge: 채팅 서버

---
+++ server/chat/tools/db.js
@@ -0,0 +1,16 @@
+const mysql2 = require("mysql2");
+const path = require("path");
+const { cb_func } = require("promisify");
+
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
+
+const db = mysql2
+  .createPool({
+    host: process.env.RDS_DB_HOST,
+    user: process.env.RDS_DB_USER,
+    password: process.env.RDS_DB_PASSWORD,
+    database: process.env.RDS_DB_DATABASE,
+  })
+  .promise();
+
+module.exports = db;
