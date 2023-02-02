6549190c41124972e27458acbabbea33554d4f32
empodi <kglein9513@gmail.com>
2023-02-02 22:16:41 +0900

    Create: Member class

--- server/user/src/test/java/com/gonggu/user/MemberApplicationTests.java
+++ server/user/src/test/java/com/gonggu/user/MemberApplicationTests.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserApplicationTests {
-
+class MemberApplicationTests {
 	@Test
 	void contextLoads() {
 	}
