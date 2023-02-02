291bb8cb27eb95e1c9dbec21c638220ef89ab599
empodi <kglein9513@gmail.com>
2023-02-02 12:30:07 +0900

    Initial Commit

--- server/user/src/main/java/com/example/demo/repository/DeviceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Device;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface DeviceRepository extends JpaRepository<Device, Long> {
-    Optional<Device> findByEndpoint(String endpoint);
-}
--- server/user/src/test/java/gonggu/user/UserApplicationTests.java
+++ server/user/src/test/java/gonggu/user/UserApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package gonggu.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class UserApplicationTests {
 
 	@Test
 	void contextLoads() {
