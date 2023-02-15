1753dfa2fa63630aef52d597b66f09d243d10b27
empodi <kglein9513@gmail.com>
2023-02-15 19:36:10 +0900

    feat: JWT 코드, Security 설정 추가

--- server/jwt/src/main/java/com/gonggu/jwt/config/SecurityConfig.java
+++ server/jwt/src/main/java/com/gonggu/jwt/config/SecurityConfig.java
@@ -1,16 +1,44 @@
 package com.gonggu.jwt.config;
 
+import com.gonggu.jwt.jwt.JwtAccessDeniedHandler;
+import com.gonggu.jwt.jwt.JwtAuthenticationEntryPoint;
+import com.gonggu.jwt.jwt.JwtSecurityConfig;
+import com.gonggu.jwt.jwt.TokenProvider;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
+@EnableMethodSecurity
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
+    private final TokenProvider tokenProvider;
+//    private final CorsFilter corsFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig(
+            TokenProvider tokenProvider,
+//            CorsFilter corsFilter,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.tokenProvider = tokenProvider;
+//        this.corsFilter = corsFilter;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer () {
+    public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().antMatchers(
                 "/h2-console/**",
                 "/favicon-ico"
@@ -20,9 +48,30 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/api/hello").permitAll()
-                .anyRequest().authenticated();
+                .csrf().disable()
+//                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeHttpRequests()
+                .antMatchers("/api/hello", "/api/authenticate", "/api/signup").permitAll()
+                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
         return http.build();
     }
 }
